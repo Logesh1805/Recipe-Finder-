2061ed153c5a3ea77e2cfcac24c6ea4544a38dac
Logesh <loki1805200@gmail.com>
2026-02-11 19:12:51 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Recipe-Finder/',   // ⚠️ your repo name
+  base: '/Recipe-Finder-/'   // <-- your repository name
 })
